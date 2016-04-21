f0b0107e582351b142f0e8f3783c97736df808aa
princessruthie <missruthie@gmail.com>
2016-04-21 08:00:56 -0400

    Chain two appends to fill out the header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,9 @@
-var name="Ruthie Princess Fields";
+var name="Ruthie P Floats";
+var role="Web Developer";
 var awesomethoughts="I am " +name +" and I am awesome.";
 var funThoughts=awesomethoughts.replace("awesome", "fun");
 console.log(funThoughts);
 $('#main').append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedName).append(formattedRole);
