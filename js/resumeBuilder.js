680779463620df092724528b04388cae622cb8cf
research-tech <fieldsru@bc.edu>
2016-04-22 14:46:34 -0400

    Use the array to populate the header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,8 +2,13 @@ var skills = ["lady swagger", "wordpress", "php", "html5/css3"];
 var bio = {
     "name" : "Princess",
     "role": "Front-End Developer",
-    "contact info": "missruthie@gmail.com",
+    "contact": "missruthie@gmail.com",
     "skills":skills,
-    "welcome message", "A hearty welcome",
-    "picUrl", "images/pic"
+    "welcome message": "A hearty welcome",
+    "picUrl": "images/pic"
 };
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contact);
+$("#header").prepend(formattedRole).prepend(formattedName);
+// $("#footerContacts").append(formattedContact);
