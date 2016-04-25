a07d560e6a1f28236e58ff5beac32c75d1610961
princessruthie <missruthie@gmail.com>
2016-04-25 08:37:47 -0400

    Practice using and mixing dot/bracket notation.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,10 +5,28 @@ var bio = {
     "contact": "missruthie@gmail.com",
     "skills":skills,
     "welcome message": "A hearty welcome",
-    "picUrl": "images/pic"
+    "picUrl": "images/dummy.jpg"
 };
+bio.name="Queen";
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contact);
 $("#header").prepend(formattedRole).prepend(formattedName);
 // $("#footerContacts").append(formattedContact);
+
+var work ={
+};
+
+work.position="research tech";
+work.employer="Thomas Jefferson University";
+work.years="Less than one";
+work.city="Philadelphia";
+
+var education={
+
+};
+education["name"]="Boston College";
+education["city"]="Chestnut Hill, Ma";
+
+$("#main").append(work["position"]).append(education["name"]);
+
